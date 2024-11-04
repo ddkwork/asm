c314c507bcb75c29e595734542623bb1f45368bb
Admin <2762713521@qq.com>
2024-11-04 18:15:39 +0800

    Test_bigNumber

---
+++ demo/demo_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"encoding/hex"
+	"testing"
+
+	"github.com/ddkwork/golibrary/mylog"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_demo(t *testing.T) {
+	data := []byte{0x9, 0x99, 0x8a, 0x7b, 0xfe, 0x46, 0xc2, 0xf0}
+	b := demo(data)
+	mylog.Struct(b)
+	assert.Equal(t, uint64(0xCD2F21A91272EE20), b.Self())
+	assert.Equal(t, mylog.Check2(hex.DecodeString("CD2F21A91272EE20")), b.Bytes())
+}
