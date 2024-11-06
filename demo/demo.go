8dd1e8b87327261d0379aa526d3d23fbc54e6a50
Admin <2762713521@qq.com>
2024-11-06 13:31:35 +0800

    init

--- demo/demo.go
+++ demo/demo.go
@@ -10,200 +10,136 @@ import (
 func demo(data []byte, t *testing.T) Bit64 {
 	var a, result, x, c, z Bit64
 	mylog.HexDump("input data", data)
-	// 0x0000F3FF
-	// 0x00000000
-	// 0x00001302
-	// 0x00000000
+
 	assert.Equal(t, uint32(0x0000F3FF), uint32(data[1])+0xf366)
 	x = mul(uint32(data[1])+0xf366, 0, 0x1302, 0)
-	assert.Equal(t, uint32(0x121DD4FE), x.Low)  // EAX
-	assert.Equal(t, uint32(0x00000000), x.High) // EDX
+	assert.Equal(t, uint32(0x121DD4FE), x.Low)
+	assert.Equal(t, uint32(0x00000000), x.High)
 
-	y := x                   //.high
-	x.Low += uint32(data[1]) // 0x121DD597
+	y := x
+	x.Low += uint32(data[1])
 
-	// 00EFF959  121DD597
-	// 00EFF95D  00000000
-	// 00EFF961  0071B793
-	// 00EFF965  00000000
 	assert.Equal(t, uint32(0x121DD597), x.Low)
 	assert.Equal(t, uint32(0x00000000), x.High)
 	x = mul(x.Low, x.High, 0x71b793, 0)
-	assert.Equal(t, uint32(0x27796b5), x.Low)   // EAX
-	assert.Equal(t, uint32(0x00080C29), x.High) // EDX
+	assert.Equal(t, uint32(0x27796b5), x.Low)
+	assert.Equal(t, uint32(0x00080C29), x.High)
 
-	z = x // 0x27796B5
+	z = x
 	z.Low += 0x7CFF86
 
 	elem2 := uint32(data[2])
 	y.Low = uint32(z.Uint64() >> 18 & 0xFFFFFFFF)
-	// stream.GenMask()
-	// data := []byte{0x9, 0x99, 0x8a, 0x7b, 0xfe, 0x46, 0xc2, 0xf0}
 
 	local10f := z.ShiftRight(18)
-	// assert.Equal(t, uint32(0x0000F3FF), x.Low)  // EAX
-	// assert.Equal(t, uint32(0x00000000), x.High) // EDX
 
-	// 0x02F4963B
-	// 0x00080C29
-	// 0x6381BE9A
-	// 0x00000000
 	assert.Equal(t, uint32(0x02F4963B), z.Low)
 	assert.Equal(t, uint32(0x00080C29), z.High)
 
-	x = div(z.Low, z.High, 0x6381be9a, 0)       // 0x14B426 感觉是少了一半的值没有存储
-	assert.Equal(t, uint32(0x14b426), x.Low)    // EAX
-	assert.Equal(t, uint32(0x00000000), x.High) // EDX
+	x = div(z.Low, z.High, 0x6381be9a, 0)
+	assert.Equal(t, uint32(0x14b426), x.Low)
+	assert.Equal(t, uint32(0x00000000), x.High)
 
 	x.Low += y.Low
 
-	//$ ==>     031EF4E3
-	//$+4       00000002
-	//$+8       00000002
-	//$+C       00000000
 	assert.Equal(t, uint32(0x031EF4E3), x.Low)
 	assert.Equal(t, uint32(0x00000002), local10f.High)
 
-	x = mul(x.Low, local10f.High, 2, 0)       // 0x63DE9C6 passed
-	assert.Equal(t, uint32(0x63de9c6), x.Low) // EAX
-	assert.Equal(t, uint32(0x4), x.High)      // EDX
+	x = mul(x.Low, local10f.High, 2, 0)
+	assert.Equal(t, uint32(0x63de9c6), x.Low)
+	assert.Equal(t, uint32(0x4), x.High)
 
-	// 006FFA75  0000F3F0
-	// 006FFA79  00000000
-	// 006FFA7D  00001634
-	// 006FFA81  00000000
-	assert.Equal(t, uint32(0x0000F3F0), elem2+0xf366) // EAX
-	c = mul(elem2+0xf366, 0, 0x1634, 0)               // 0x15282CC0 passed
-	assert.Equal(t, uint32(0x15282cc0), c.Low)        // EAX
-	assert.Equal(t, uint32(0x00000000), c.High)       // EDX
+	assert.Equal(t, uint32(0x0000F3F0), elem2+0xf366)
+	c = mul(elem2+0xf366, 0, 0x1634, 0)
+	assert.Equal(t, uint32(0x15282cc0), c.Low)
+	assert.Equal(t, uint32(0x00000000), c.High)
 
 	var5 := c.Low + elem2
-	// 0x15282D4B
-	// 0x00000000
-	// 0x02F4963B
-	// 0x00080C29
+
 	assert.Equal(t, uint32(0x15282D4B), var5+1)
 	assert.Equal(t, uint32(0x00000000), y.High+c.High)
 	assert.Equal(t, uint32(0x02F4963B), z.Low)
 	assert.Equal(t, uint32(0x00080C29), z.High)
 
 	c = mul(var5+1, y.High+c.High, z.Low, z.High)
-	assert.Equal(t, uint32(0x2c486249), c.Low)  // EAX
-	assert.Equal(t, uint32(0xb0254c13), c.High) // EDX
+	assert.Equal(t, uint32(0x2c486249), c.Low)
+	assert.Equal(t, uint32(0xb0254c13), c.High)
 
-	c.High += 4 //?? why // todo bug
+	c.High += 4
 
 	z.Low = c.Low
 	z.High = c.High
 	z.Low += x.Low + 0x2d1f65
 	uVar11 := z
-	// uVar11.High = uVar11.Low //
 
 	var7 := uint32(data[3])
 	r := uVar11.ShiftRight(18)
-	// 0x32B36B74
-	// 0xB0254C17
-	// 0x6381BE9A
-	// 0x00000000
-	assert.Equal(t, uint32(0x32B36B74), uVar11.Low)  // EAX
-	assert.Equal(t, uint32(0xB0254C17), uVar11.High) // EDX
-	x = div(uVar11.Low, uVar11.High, 0x6381be9a, 0)
-	assert.Equal(t, uint32(0xc52afd7b), x.Low) // EAX
-	assert.Equal(t, uint32(0x1), x.High)       // EDX
 
-	// x.Low += (uVar10 << 32) | (var5 | elem2) + 0x21d78d
+	assert.Equal(t, uint32(0x32B36B74), uVar11.Low)
+	assert.Equal(t, uint32(0xB0254C17), uVar11.High)
+	x = div(uVar11.Low, uVar11.High, 0x6381be9a, 0)
+	assert.Equal(t, uint32(0xc52afd7b), x.Low)
+	assert.Equal(t, uint32(0x1), x.High)
+
 	x.Add(r)
 	x.AddInt(0x21d78d)
-	// 006FFA75  1852A1B4
-	// 006FFA79  00002C0B
-	// 006FFA7D  00000003
-	// 006FFA81  00000000
-	assert.Equal(t, uint32(0x1852A1B4), x.Low)  // EAX
-	assert.Equal(t, uint32(0x00002C0B), x.High) // EDX
-	x = mul(x.Low, x.High, 3, 0)
-	assert.Equal(t, uint32(0x48f7e51c), x.Low) // EAX
-	assert.Equal(t, uint32(0x8421), x.High)    // EDX
 
-	//$ ==>     0000F3E1
-	//$+4       00000000
-	//$+8       00001968
-	//$+C       00000000
+	assert.Equal(t, uint32(0x1852A1B4), x.Low)
+	assert.Equal(t, uint32(0x00002C0B), x.High)
+	x = mul(x.Low, x.High, 3, 0)
+	assert.Equal(t, uint32(0x48f7e51c), x.Low)
+	assert.Equal(t, uint32(0x8421), x.High)
+
 	assert.Equal(t, uint32(0x0000F3E1), var7+0xf366)
 	c = mul(var7+0xf366, 0, 0x1968, 0)
-	assert.Equal(t, uint32(0x18340c68), c.Low)  // EAX
-	assert.Equal(t, uint32(0x00000000), c.High) // EDX
+	assert.Equal(t, uint32(0x18340c68), c.Low)
+	assert.Equal(t, uint32(0x00000000), c.High)
 
 	y.Low = c.Low
 	var5 = c.Low + var7
-	//$ ==>     18340CE4
-	//$+4       00000000
-	//$+8       32B36B74
-	//$+C       B0254C17
+
 	assert.Equal(t, uint32(0x18340CE4), var5+1)
-	assert.Equal(t, uint32(0x00000000), y.High) // todo test more data
+	assert.Equal(t, uint32(0x00000000), y.High)
 	assert.Equal(t, uint32(0x32B36B74), z.Low)
 	assert.Equal(t, uint32(0xB0254C17), z.High)
-	// 00A21895   | E8  | call <asm.__allmul>                                   | main.c:193
+
 	c = mul(var5+1, y.High, z.Low, z.High)
-	assert.Equal(t, uint32(0x44652350), c.Low)  // EAX
-	assert.Equal(t, uint32(0x423ff98e), c.High) // EDX
+	assert.Equal(t, uint32(0x44652350), c.Low)
+	assert.Equal(t, uint32(0x423ff98e), c.High)
 
 	z.Low = x.Low + c.Low
 	z.High = c.High + x.High
-	//$ ==>     8D5D086C
-	//$+4       42407DAF
-	//$+8       6381BE9A
-	//$+C       00000000
+
 	assert.Equal(t, uint32(0x8D5D086C), z.Low)
 	assert.Equal(t, uint32(0x42407DAF), z.High)
 	x = div(z.Low, z.High, 0x6381BE9A, 0)
-	// 00A218C7   | E8  | call <asm.__alldiv>                                   | main.c:210
-	assert.Equal(t, uint32(0xaa720dbb), x.Low)  // EAX
-	assert.Equal(t, uint32(0x00000000), x.High) // EDX
+
+	assert.Equal(t, uint32(0xaa720dbb), x.Low)
+	assert.Equal(t, uint32(0x00000000), x.High)
 
 	back := z.Low
 	z.Low = x.Low + (z.High | a.High)
 	high := uint32(data[4])
-	//$ ==>     0000F464
-	//$+4       00000000
-	//$+8       00001C9E
-	//$+C       00000000
+
 	assert.Equal(t, uint32(0x0000F464), high+0xf366)
 	x = mul(high+0xf366, 0, 0x1c9e, 0)
-	assert.Equal(t, uint32(0x1b51c5b8), x.Low)  // EAX
-	assert.Equal(t, uint32(0x00000000), x.High) // EDX
+	assert.Equal(t, uint32(0x1b51c5b8), x.Low)
+	assert.Equal(t, uint32(0x00000000), x.High)
 
 	var5 = x.Low + high
 	y = Bit64{Low: x.High, High: high}
-	//$ ==>     1B51C6B7
-	//$+4       00000000
-	//$+8       8D5D086C
-	//$+C       42407DAF
+
 	assert.Equal(t, uint32(0x1B51C6B7), var5+1)
 	assert.Equal(t, uint32(0x00000000), y.Low)
 	assert.Equal(t, uint32(0x8D5D086C), back)
-	assert.Equal(t, uint32(0x42407DAF), z.High) //?
+	assert.Equal(t, uint32(0x42407DAF), z.High)
 
 	x = mul(var5+1, y.Low, back, z.High)
-	assert.Equal(t, uint32(0x10308d34), x.Low)  // EAX
-	assert.Equal(t, uint32(0x85c42a27), x.High) // EDX
+	assert.Equal(t, uint32(0x10308d34), x.Low)
+	assert.Equal(t, uint32(0x85c42a27), x.High)
 	return x
 
-	//todo 00A2192D   | 6A  | push 0                                                | main.c:242
-	//这里开始还有个乘法猜到循环的
-
-	// push 0
-	// x.High
-	// push 1090?
-	// push 4
-	// x.Low
-
-	// 00B2FB0D  C9DDF112
-	// 00B2FB11  00001090
-	// 00B2FB15  00000004
-	// 00B2FB19  00000000
-
-	x.Low += 0x2d1f65 // todo test
+	x.Low += 0x2d1f65
 	x.High = 0
 	result = x
 	c = x
@@ -220,9 +156,9 @@ func demo(data []byte, t *testing.T) Bit64 {
 		c = x
 		elem := data[j]
 		c = mul(uint32(j*j), uint32(j*j>>31), k.High, k.High)
-		y.Low = c.High // low
+		y.Low = c.High
 		x = mul(uint32((elem)+1), uint32(int(elem)>>31+i), result.High, v)
-		// todo missing a div ?
+
 		i--
 	}
 	return result
@@ -246,9 +182,9 @@ func TestRange() {
 		c = x
 		elem := data[j]
 		c = mul(uint32(j*j), uint32(j*j>>31), k.High, k.High)
-		y.Low = c.High // low
+		y.Low = c.High
 		x = mul(uint32((elem)+1), uint32(int(elem)>>31+i), result.High, v)
-		// todo missing a div ?
+
 		i--
 	}
 }
--- demo/demo_test.go
+++ demo/demo_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/hex"
 	"testing"
 
+	"github.com/ddkwork/golibrary/stream/binary"
+
 	"github.com/ddkwork/golibrary/mylog"
 	"github.com/stretchr/testify/assert"
 )
@@ -11,9 +13,47 @@ import (
 func Test_demo(t *testing.T) {
 	data := []byte{0x9, 0x99, 0x8a, 0x7b, 0xfe, 0x46, 0xc2, 0xf0}
 	b := demo(data, t)
-	b = b
-	return
-	mylog.Struct("demo", b)
 	assert.Equal(t, uint64(0xCD2F21A91272EE20), b.Uint64())
 	assert.Equal(t, mylog.Check2(hex.DecodeString("CD2F21A91272EE20")), b.Bytes())
+	assert.Equal(t, mylog.Check2(hex.DecodeString("CD2F21A91272EE20")), asm(data))
+}
+
+func asm(data []byte) []byte {
+	v5 := 0
+	v6 := 0
+	v7 := 0
+	v8 := 0
+	v9 := 0
+	index := 0
+	i := 0
+	v17 := 0
+	v18 := 0
+	v20 := 0
+	out := 0
+
+	out = 0x71B793*(int(data[1])+0x1302*(int(data[1])+0xF0)) + 0x7CFF86
+	v5 = (int(data[2])) + 0x1634*(int(data[2])+0xF0) + 1
+	v6 = (v5*(out) + 2*((out)/0x6381BE9A+(out>>0x12)) + 0x2D1F65) >> 0x20
+	out = v5*(out) + 2*((out)/0x6381BE9A+(out>>0x12)) + 0x2D1F65
+	v6 = out
+	v7 = ((int(data[3]))+0x1968*(int(data[3])+0xF0)+1)*(v6) + 3*((v6)/0x6381BE9A+(v6>>0x12)+0x21D78D)
+	v17 = v7/0x6381BE9A + (v7 >> 0x12)
+	v8 = ((int(data[4])) + 0x1C9E*(int(data[4])+0xF0) + 1) * v7
+	v9 = 4*(v17) + v8 + 0xB47D9D
+	index = 7
+	out = (4*(v17)+v8+0xB47D9D)&0xFFF0 + (v9 >> 0x13) + v9&0xFFFFFF + v9&0xFFFFFF
+	i = 7
+	v20 = 0x18F - (int(data[7]))
+	for {
+		v18 = int(data[index])
+		v17 = (out / 0x6A) + (((v6>>32)^((v6>>32)>>7))<<32|((v6)^(v6>>7)))>>0x19
+		out = v18*v18*v18 + ((v18)+1)*(out) + (index * index * (v17))
+		index = i - 1
+		out += v17/0x14C9 + (i)*(int(data[i]))*(int(data[i]))*(i+v20)
+		i--
+		if i-1 < 0 {
+			break
+		}
+	}
+	return binary.LittleEndian.PutUint64(uint64(out))
 }
