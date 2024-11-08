877fd70ce0e3bc09e6ee20b85b847d3b04736656
Admin <2762713521@qq.com>
2024-11-09 00:10:19 +0800

    init

--- demo/demo_test.go
+++ demo/demo_test.go
@@ -14,7 +14,10 @@ func Test_demo(t *testing.T) {
 	b := demo(data, t)
 	assert.Equal(t, 0xCD2F21A91272EE20, b.Uint64())
 	assert.Equal(t, mylog.Check2(hex.DecodeString("CD2F21A91272EE20")), b.Bytes())
-	return
+}
+
+func TestName(t *testing.T) {
+	data := []byte{0x9, 0x99, 0x8a, 0x7b, 0xfe, 0x46, 0xc2, 0xf0}
 	assert.Equal(t, mylog.Check2(hex.DecodeString("CD2F21A91272EE20")), asm(data))
 }
 
@@ -46,10 +49,7 @@ func asm(data []byte) []byte {
 	index = 7
 	out = (4*(v17)+v8+0xB47D9D)&0xFFF0 + (v9 >> 0x13) + v9&0xFFFFFF + v9&0xFFFFFF // 0xBBC1935A1FC
 	i = 7
-	v20 = 0x18F - (int(data[7])) // 0xC90B9788
-	// v6 0xB0254C1732B36B74
-	// out 0xBBC1935A1FC
-	// elem 0
+	v20 = 0x18F
 	v20 = 0xC90B9788
 	out = 0x13530849000010B9
 	v6 = 0x1353084910B9
@@ -58,8 +58,9 @@ func asm(data []byte) []byte {
 		v17 = (out / 0x6A) + (((v6>>32)^((v6>>32)>>7))<<32|((v6)^(v6>>7)))>>0x19 // 0x43F9B1AC1A
 		out = elem*elem*elem + ((elem)+1)*(out) + (index * index * (v17))        // 0xB19168E94E0DC
 		index = i - 1                                                            // 6
-		out += v17/0x14C9 + (i)*(int(data[i]))*(int(data[i]))*(i+v20)            // 0x3BF80DB8 000FC5F6 todo must
-		i--                                                                      // 6
+		out += v17/0x14C9 + i*(int(data[i]))*(int(data[i]))*v20                  // 0x3BF80DB8 000FC5F6 todo must
+		i--
+		v20--
 		if i-1 < 0 {
 			break
 		}
